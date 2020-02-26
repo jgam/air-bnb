e2a7e874c2509cd154f089981097634838d1ee86
jgam <jgam@alumni.nd.edu>
2020-02-26 23:01:17 +0900

    User app done with admin panel control

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,34 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
 
 # Register your models here.
 @admin.register(models.User)
-class CustomUserAdmin(admin.ModelAdmin):
+class CustomUserAdmin(UserAdmin):
+    """Custom User Admin"""
+
+    """
+    # list display shows displays of the users in admin panel
+    list_display = ('username', "email", 'gender',
+                    'language', 'currency', 'superhost')
+    # list filter shows the filtering options for us (automatic from webframework)
+    list_filter = ("currency", "language", "superhost",)
+    """
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "language",
+                    "currency",
+                    "superhost",
+                )
+            }
+        ),
+    )
     pass

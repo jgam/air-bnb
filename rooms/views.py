949a63f7df2a8ade2d8856f1ae2f9aab0f61e4c4
jgam <jgam@alumni.nd.edu>
2020-03-01 19:50:38 +0900

    class based views

--- rooms/views.py
+++ rooms/views.py
@@ -1,29 +1,13 @@
-from math import ceil
-from django.shortcuts import render, redirect
-from django.core.paginator import Paginator, EmptyPage
+from django.views.generic import ListView
 from . import models
 
 # Create your views here.
 
 
-def all_rooms(request):
-    page = request.GET.get("page", 1)
-    # this is conventional way
-    # page = int(page or 1)
-    # page_size = 10
-    # limit = page_size * page
-    # offset = limit - page_size
-    # all_rooms = models.Room.objects.all()[offset:limit]
-    # page_count = ceil(models.Room.objects.count() / page_size)
-    #return render(request, "rooms/home.html", context={"rooms": all_rooms, "page":page, "page_count":page_count, "page_range": range(1, page_count+1) })
-    
-    # Django way
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10, orphans=5)
-    try:
-        rooms = paginator.get_page(int(page))
-    except EmptyPage:
-        rooms = paginator.page(1)
-        return redirect("/")
+class HomeView(ListView):
 
-    return render(request, "rooms/home.html", context={"rooms": rooms })
+    """HomeView Class Definition"""
+    model = models.Room
+    paginate_by = 10
+    ordering = "created"
+    paginate_orphans = 5

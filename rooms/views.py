f7274732655b10e8093d1ac4dc0b384925cc17b9
jgam <jgam@alumni.nd.edu>
2020-03-02 14:19:44 +0900

    form being done

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,7 @@
 from django.utils import timezone
 from django.views.generic import ListView
 from django.shortcuts import render, redirect
+from django_countries import countries
 from django.http import Http404
 from django.urls import reverse
 from . import models
@@ -32,3 +33,45 @@ def room_detail(request, pk):
     except models.Room.DoesNotExist:
         raise Http404()
         # return redirect(reverse("core:home"))
+
+
+def search(request):
+    city = request.GET.get("city", "anywhere")
+    city = str.capitalize(city)
+    country = request.GET.get("country", "KR")
+    room_type = int(request.GET.get("country", 0))
+
+    price = int(request.GET.get("price", 0))
+    guests = int(request.GET.get("guests", 0))
+    bedrooms = int(request.GET.get("bedrooms", 0))
+    beds = int(request.GET.get("beds", 0))
+    baths = int(request.GET.get("baths", 0))
+    s_amenities = request.GET.getlist("amenities")
+    s_facilities = request.GET.getlist("facilities")
+
+    form = {
+        "city": city,
+        "s_room_type": room_type,
+        "s_country": country,
+        "price": price,
+        "guests": guests,
+        "bedrooms": bedrooms,
+        "beds": beds,
+        "baths": baths
+    }
+
+    room_types = models.RoomType.objects.all()
+    amenities = models.Amenity.objects.all()
+    facilities = models.Facility.objects.all()
+
+    choices = {
+        "countries": countries,
+        "room_types": room_types,
+        "amenities": amenities,
+        "facilities": facilities
+    }
+
+    return render(request, "rooms/search.html", {
+        **form,
+        **choices
+    })

2f0e6c600ccb8d051a60021253538430e30b3101
jgam <jgam@alumni.nd.edu>
2020-03-02 11:20:52 +0900

    detail view can be done

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,7 @@
 from django.utils import timezone
 from django.views.generic import ListView
 from django.shortcuts import render, redirect
+from django.http import Http404
 from django.urls import reverse
 from . import models
 
@@ -29,4 +30,5 @@ def room_detail(request, pk):
         room = models.Room.objects.get(pk=pk)
         return render(request, "rooms/detail.html", {"room": room})
     except models.Room.DoesNotExist:
-        return redirect(reverse("core:home"))
+        raise Http404()
+        # return redirect(reverse("core:home"))

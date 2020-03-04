f81b730a60fc060dfe689b8e3878b84c4fcd8f9f
jgam <jgam@alumni.nd.edu>
2020-03-04 13:25:09 +0900

    login and logout using easier way

--- users/views.py
+++ users/views.py
@@ -1,12 +1,30 @@
 from django.shortcuts import render, redirect, reverse
+from django.urls import reverse_lazy
 from django.views import View
 from . import forms
 from django.contrib.auth import authenticate, login, logout
+from django.views.generic import FormView
 
 # Create your views here.
 
 
-class LoginView(View):
+class LoginView(FormView):
+    template_name = "users/login.html"
+    form_class = forms.LoginForm
+    success_url = reverse_lazy("core:home")
+    initial = {
+        "email": "jgam@alumni.nd.edu"
+    }
+
+    def form_valid(self, form):
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+            return super().form_valid(form)
+
+    """
     def get(self, request):
         form = forms.LoginForm()
         return render(request, "users/login.html", {"form": form})
@@ -21,6 +39,8 @@ class LoginView(View):
                 login(request, user)
                 return redirect(reverse("core:home"))
         return render(request, "users/login.html", {"form": form})
+    """
+
 
 def log_out(request):
     logout(request)

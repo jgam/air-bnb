05c3ea7803f50d9870dc6a5f9a44347eced20cb8
jgam <jgam@alumni.nd.edu>
2020-03-08 16:20:51 +0900

    rooms done with tailwind

--- rooms/views.py
+++ rooms/views.py
@@ -13,7 +13,7 @@ class HomeView(ListView):
 
     """HomeView Class Definition"""
     model = models.Room
-    paginate_by = 10
+    paginate_by = 12
     ordering = "created"
     paginate_orphans = 5
     context_object_name = "rooms"
